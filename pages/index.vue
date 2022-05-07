c3144eeff9ca0493c0a696895a527e5b60777634
simone98dm <simone.dalmas@outlook.it>
2022-05-07 08:26:49 +0200

    do stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,31 @@
 <template>
   <div
-    class="container mx-auto items-center justify-center content-center max-width-sm text-center"
+    class="
+      container
+      mx-auto
+      items-center
+      justify-center
+      content-center
+      max-width-sm
+      text-center
+    "
   >
     <span>{{ text }}</span>
     <button
-      class="border-2 border-purple-800 text-black px-4 py-2 rounded-md text-1xl font-medium hover:bg-purple-600 transition duration-300 hover:text-white text-4xl"
+      class="
+        border-2 border-purple-800
+        text-black
+        px-4
+        py-2
+        rounded-md
+        text-1xl
+        font-medium
+        hover:bg-purple-600
+        transition
+        duration-300
+        hover:text-white
+        text-4xl
+      "
       @click="callTheAlarm"
     >
       PANIC!!
@@ -13,6 +34,7 @@
 </template>
 
 <script lang="ts">
+import { AxiosResponse } from 'axios'
 import Vue from 'vue'
 
 export default Vue.extend({
@@ -21,7 +43,20 @@ export default Vue.extend({
     text: '',
   },
   methods: {
-    callTheAlarm() {},
+    async callTheAlarm() {
+      const available = await this.$axios
+        .get(
+          'https://raw.githubusercontent.com/simone98dm/panic-button/master/panic/single/README.md'
+        )
+        .then((response) => response.data)
+        .then((data: string) => data.split('\n'))
+
+      const random = this.generateRandom(available.length)
+      this.text = available[random]
+    },
+    generateRandom(limit: number) {
+      return Math.floor(Math.random() * (limit + 1))
+    },
   },
 })
 </script>
