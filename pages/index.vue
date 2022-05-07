c8895727cbc74db37674d71460aeabe8f2ff96ac
simone98dm <simone.dalmas@outlook.it>
2022-05-07 08:15:09 +0200

    project init

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="
+      container
+      mx-auto
+      items-center
+      justify-center
+      content-center
+      max-width-sm
+      text-center
+    "
+  >
+    <span>{{ text }}</span>
+    <button
+      class="
+        border-2 border-purple-800
+        text-black
+        px-4
+        py-2
+        rounded-md
+        text-1xl
+        font-medium
+        hover:bg-purple-600
+        transition
+        duration-300
+        hover:text-white
+        text-4xl
+      "
+      @click="callTheAlarm"
+    >
+      PANIC!!
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data: {
+    text: '',
+  },
+  methods: {
+    callTheAlarm() {
+    }
+  }
+})
+</script>
---
+++ panic/single/README.md
@@ -0,0 +1,6 @@
+Cosa fai nella vita
+Studi all'università
+Come hai passato questi 2 anni di pandemia
+Che genere di musica ti piace
+Che genere di film ti piace guardare
+Ultima serie vista su netflix?
