97977bfe146b004b54a5595072836d74c0f58f30
simone98dm <simone.dalmas@outlook.it>
2022-05-14 12:38:09 +0200

    feat: :sparkles: add emoticons component

---
+++ components/deep/BaseEmoticon/BaseEmoticon.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @click="clickHandler" style="display: inline-block">
+    <rocket v-if="this.icon === 'rocket'" />
+    <panic v-if="this.icon === 'panic'" />
+    <pizza v-if="this.icon === 'pizza'" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import Rocket from '~/components/deep/BaseEmoticon/Emoticons/Rocket.vue'
+import Panic from '~/components/deep/BaseEmoticon/Emoticons/Panic.vue'
+import Pizza from '~/components/deep/BaseEmoticon/Emoticons/Pizza.vue'
+
+enum Icons {
+  ROCKET = 'rocket',
+  PANIC = 'panic',
+  PIZZA = 'pizza',
+}
+
+export default Vue.extend({
+  name: 'BaseEmoticonComponent',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    clickHandler() {
+      this.$emit('onclick')
+    },
+  },
+  components: { Rocket, Panic, Pizza },
+})
+</script>
---
+++ components/deep/BaseEmoticon/Emoticons/Panic.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>😰</div>
+</template>
