c30a281dfe16cb09ea0ed6bdbec5cdf939e68d17
simone98dm <simone.dalmas@outlook.it>
2022-05-14 10:01:33 +0200

    chore: fix default route

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: { 
-    base: '/pani-button/'
+    base: '/panic-button/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
