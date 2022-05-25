e79fe6c8694dfffd7aa25443742a7040188029db
Simone Dal Mas <simonedalmas@MacBook-Air-di-Simone.local>
2022-05-25 08:11:39 +0200

    feat: :sparkles: add loading component

---
+++ components/deep/Loading/Loading.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="fade-wrapper" v-if="show">
+    <div class="content">
+      <div class="loading"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'LoadingComponent',
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
+
+<style scoped>
+#fade-wrapper {
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.9);
+  overflow-x: hidden;
+  transition: 0.5s;
+}
+
+.content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  transform: translate(-50%, -50%);
+}
+
+.loading {
+  margin: 0 16px;
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #ca34db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
