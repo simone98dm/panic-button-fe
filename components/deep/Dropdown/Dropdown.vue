4bfa4fa0dc6f8d5243b545d1002231559d894b84
simone98dm <simone.dalmas@outlook.it>
2022-05-25 13:38:07 +0200

    feat: :sparkles: add dropdown&language components

---
+++ components/deep/Dropdown/Dropdown.vue
@@ -0,0 +1,21 @@
+<template>
+    <select>
+      <option value="en">EN</option>
+      <option value="it">IT</option>
+    </select>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'DropdownComponent'
+})
+</script>
+
+<style scoped>
+select {
+    padding: 5px 10px;
+    border: 1px solid grey;
+    border-radius: 5px;
+}
+</style>
---
+++ components/high/Language/Language.vue
@@ -0,0 +1,12 @@
+<template>
+  <Dropdown></Dropdown>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Dropdown from '~/components/deep/Dropdown/Dropdown.vue'
+export default Vue.extend({
+  name: 'LanguageComponent',
+  components: { Dropdown },
+})
+</script>
