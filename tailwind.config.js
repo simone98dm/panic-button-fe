914d1982587a6d88ae04f6d3255b1e6bb8faa12e
simone98dm <simone.dalmas@outlook.it>
2022-06-04 09:54:10 +0200

    chore: add variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ module.exports = {
     extend: {},
   },
   variants: {
-    backgroundColor: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd']
+    
+  variants: {
+    backgroundColor: ["dark", "dark-hover", "dark-group-hover", "dark-even", "dark-odd", "hover", "responsive"],
+    borderColor: ["dark", "dark-focus", "dark-focus-within", "hover", "responsive"],
+    textColor: ["dark", "dark-hover", "dark-active", "hover", "responsive"]
   },
   plugins: [require('tailwindcss-dark-mode')()],
 }
