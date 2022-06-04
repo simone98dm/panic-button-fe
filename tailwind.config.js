35a4c584ca85cbc9a8e21167b83d0a658b2d08a9
simone98dm <simone.dalmas@outlook.it>
2022-06-04 09:58:02 +0200

    fix: format json config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,23 @@ module.exports = {
     extend: {},
   },
   variants: {
-    
-  variants: {
-    backgroundColor: ["dark", "dark-hover", "dark-group-hover", "dark-even", "dark-odd", "hover", "responsive"],
-    borderColor: ["dark", "dark-focus", "dark-focus-within", "hover", "responsive"],
-    textColor: ["dark", "dark-hover", "dark-active", "hover", "responsive"]
+    backgroundColor: [
+      'dark',
+      'dark-hover',
+      'dark-group-hover',
+      'dark-even',
+      'dark-odd',
+      'hover',
+      'responsive',
+    ],
+    borderColor: [
+      'dark',
+      'dark-focus',
+      'dark-focus-within',
+      'hover',
+      'responsive',
+    ],
+    textColor: ['dark', 'dark-hover', 'dark-active', 'hover', 'responsive'],
   },
   plugins: [require('tailwindcss-dark-mode')()],
 }
